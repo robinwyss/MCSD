ece5e6dc31bf4d664d1c759652ca4a64d8a41d07
Robin Wyss <rwyss@configit.com>
2015-06-21 21:18:37 +0200

    created VS2012 solution, formatted code

---
+++ 483/1 Manage program flow/1.2/Locking.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCSD._1_Manage_program_flow._1._2 {
+  internal class Locking {
+    public static void WithoutLocking() {
+      var c = 0;
+
+      var task = Task.Run(
+        () => {
+          for ( int i = 0; i < 10000000; i++ ) {
+            c++;
+          }
+        } );
+      for ( int i = 0; i < 10000000; i++ ) {
+        c--;
+      }
+      task.Wait();
+      Console.WriteLine( "Count without locking: {0}", c );
+    }
+
+    public static void WithLocking() {
+      var c = 0;
+
+      var lok = new object();
+
+      var task = Task.Run(
+        () => {
+          for ( int i = 0; i < 10000000; i++ ) {
+            lock ( lok ) {
+              c++;
+            }
+          }
+        } );
+      for ( int i = 0; i < 10000000; i++ ) {
+        lock ( lok ) {
+          c--;
+        }
+      }
+      task.Wait();
+      Console.WriteLine( "Count with locking: {0}", c );
+    }
+  }
+}
--- 483/Program.cs
+++ 483/Program.cs
@@ -1,24 +1,34 @@
 using System;
 
-namespace MCSD
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("MCSD");
-            //Threads.StartThread();
-            //ThreadPools.RunFromPool();
-            Tasks.RunTask();
-            Tasks.TaskWithChildren();
-            ParallelClass.For();
-            ParallelClass.ForEach();
-            ParallelClass.Break();
-            ParallelClass.Stop();
-            var asyncTask = AsyncAwait.DoSomethingAsync();
-            Console.WriteLine("reading text");
-            Console.WriteLine(asyncTask.Result);
-            Console.WriteLine("Done");
-        }
+using MCSD._1_Manage_program_flow._1._2;
+
+namespace MCSD {
+  public class Program {
+    public static void Main( string[] args ) {
+      Console.WriteLine( "MCSD" );
+      //Run11();
+      Run12();
+      Console.WriteLine( "Done. Press any key to exit..." );
+      Console.ReadLine();
     }
+
+    private static void Run12() {
+      Locking.WithoutLocking();
+      Locking.WithLocking();
+    }
+
+    private static void Run11() {
+      Threads.StartThread();
+      ThreadPools.RunFromPool();
+      Tasks.RunTask();
+      Tasks.TaskWithChildren();
+      ParallelClass.For();
+      ParallelClass.ForEach();
+      ParallelClass.Break();
+      ParallelClass.Stop();
+      var asyncTask = AsyncAwait.DoSomethingAsync();
+      Console.WriteLine( "reading text" );
+      Console.WriteLine( asyncTask.Result );
+    }
+  }
 }
